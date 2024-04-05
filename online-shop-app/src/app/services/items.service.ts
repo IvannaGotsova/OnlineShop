6fc16dee789dbfc7a2a268b97e6d5bf9ab63bbc8
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-05 21:30:50 +0300

    Modiied getItems

--- online-shop-app/src/app/services/items.service.ts
+++ online-shop-app/src/app/services/items.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs/internal/Observable';
+import { Items, PaginationParams } from '../../types';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,10 @@ export class ItemsService {
 
   constructor(private ApiService: ApiService) { }
 
-  getItems = (url:string, params: any): Observable<any> => {
-    return this.ApiService.get(url, params);
+  getItems = (url:string, params: PaginationParams): Observable<Items> => {
+    return this.ApiService.get(url, {
+      params,
+      responseType: "json",
+  });
   }
 }
