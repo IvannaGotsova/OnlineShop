c49f421c569b020608cea7874fe835609ccf0df2
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-05 18:55:33 +0300

    Added imports and component

--- online-shop-app/src/app/components/home/home.component.ts
+++ online-shop-app/src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
 })
 export class HomeComponent {
 
