b52c743745f50c7ac3edd1892c916c63bc7b7312
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-05 20:06:26 +0300

    Added HTTPClient and URL

--- online-shop-app/src/app/services/api.service.ts
+++ online-shop-app/src/app/services/api.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  
+  constructor(
+    private HTTPClient: HttpClient) { }
+
+    get<T>(url:string, options?: any): Observable<T>{
+      return this.HTTPClient.get<T>(url, options) as Observable<T>;
+    } 
 }
