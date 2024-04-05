d1f4a391d6c6e1c85a47c3cd6eb62c0bf0df9e9d
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-05 20:37:11 +0300

    Changed get

--- online-shop-app/src/app/services/api.service.ts
+++ online-shop-app/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Options } from '../../types';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ApiService {
   constructor(
     private HTTPClient: HttpClient) { }
 
-    get<T>(url:string, options?: any): Observable<T>{
+    get<T>(url:string, options: Options): Observable<T>{
       return this.HTTPClient.get<T>(url, options) as Observable<T>;
     } 
 }
