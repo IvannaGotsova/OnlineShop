3c349427991a1305add9c28ee8531854af28fbca
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-05 20:37:32 +0300

    Added function getItems

--- online-shop-app/src/app/services/items.service.ts
+++ online-shop-app/src/app/services/items.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor() { }
+  constructor(private ApiService: ApiService) { }
+
+  getItems = (url:string, params: any): Observable<any> => {
+    return this.ApiService.get(url, params);
+  }
 }
