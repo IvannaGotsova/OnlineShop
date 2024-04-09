a9ce447569c5bfb7659e3694b3ce5fc13f628918
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-09 16:55:05 +0300

    Sync 0409/24

--- online-shop-app/src/app/components/home/home.component.ts
+++ online-shop-app/src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemsService } from '../../services/items.service';
-import { Items } from '../../../types';
+import { Item, Items } from '../../../types';
+import { ItemComponent } from '../item/item.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 
@@ -17,11 +18,14 @@ export class HomeComponent {
 
   constructor(private itemService: ItemsService) {}
 
+  items: Item[] = [];
+
   ngOnInit() {
     this.itemService
     .getItems('http://localhost:3000/items', {page: 0, perPage: 5})
     .subscribe((itemsExample: Items) => {
-      console.log(itemsExample.items);
+      this.items = itemsExample.items;
     });
   }
+
 }
--- online-shop-app/src/app/components/item/item.component.ts
+++ online-shop-app/src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../../../types';
 
 @Component({
   selector: 'app-item',
@@ -9,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
+  @Input() item!: Item;
 }
