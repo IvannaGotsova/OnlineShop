bde98458c937bda41370cbf377b0fa758d26b929
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-14 13:20:03 +0300

    Modified main component

--- onlineshop/src/main.ts
+++ onlineshop/src/main.ts
@@ -1,6 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Footer } from './components/footer.component';
+import { Header } from './components/header.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, Header, Footer],
+  template: `
+  <app-header></app-header>
+
+   <div class="image-container">
+      <img src="assets/Picture.jpg" alt="Image" />
+      <img src="assets/Picture.jpg" alt="Image" />
+      <img src="assets/Picture.jpg" alt="Image" />
+    </div>
+
+  <app-footer></app-footer>
+  `
+})
+export class App {
+}
+
+bootstrapApplication(App, {
+  providers: [],
+});
