404e4f8e48c55d6f6524d67b26abe5f60e86f0e1
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-15 11:21:41 +0300

    Initial routing

--- onlineshop/src/app/app-routing.module.ts
+++ onlineshop/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { App } from '../main';
-import { AromaSticks } from '../components/aromasticks.component';
-import { Candles } from '../components/candles.component';
-import { Diffusers } from '../components/diffusers.component';
-import { Others } from '../components/others.component';
-import { Contacts } from '../components/contacts.component';
-import { AboutUs } from '../components/aboutus.component';
+import { AppComponent } from './app.component';
+import { AromaSticks } from '../components/aromasticks/aromasticks.component';
+import { Candles } from '../components/candles/candles.component';
+import { Diffusers } from '../components/diffusers/diffusers.component';
+import { Others } from '../components/others/others.component';
+import { Contacts } from '../components/contacts/contacts.component';
+import { AboutUs } from '../components/aboutus/aboutus.component';
 
 
 const routes: Routes = [
-  { path: '', component: App },
+  { path: '', component: AppComponent },
   { path: 'aromasticks', component: AromaSticks },
   { path: 'candles', component: Candles },
   { path: 'diffusers', component: Diffusers },
   { path: 'others', component: Others },
   { path: 'contacts', component: Contacts },
   { path: 'aboutus', component: AboutUs },
+  { path: '*', component: AppComponent }
 ];
 
 @NgModule({
