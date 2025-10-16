de219a62e166123f44119f7bbee05f503d4d253c
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-16 20:11:29 +0300

    Add router outlet

--- onlineshop/src/components/header/header.component.ts
+++ onlineshop/src/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from '../../app/app-routing.module.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AppRoutingModule],
+  imports: [AppRoutingModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
