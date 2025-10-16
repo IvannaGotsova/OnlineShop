45fd33fdead69ffe1088495632fb42a1e9d9bdf4
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-16 19:21:13 +0300

    Added initial settings

--- onlineshop/src/main.ts
+++ onlineshop/src/main.ts
@@ -1,30 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Footer } from './components/footer.component';
-import { Header } from './components/header.component';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, Header, Footer],
-  template: `
-  <app-header></app-header>
-
-   <div class="image-container">
-      <img src="assets/Picture.jpg" alt="Image" />
-      <img src="assets/Picture.jpg" alt="Image" />
-      <img src="assets/Picture.jpg" alt="Image" />
-    </div>
-
-  <app-footer></app-footer>
-  `
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
 })
-export class App {
-}
-
-bootstrapApplication(App, {
-  providers: [],
-});
+  .catch(err => console.error(err));
