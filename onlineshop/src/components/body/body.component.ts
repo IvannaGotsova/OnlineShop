801df57a1900d5d0c2016c3dc1ef5a356859aea4
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-17 23:21:34 +0300

    Sync

--- onlineshop/src/components/body/body.component.ts
+++ onlineshop/src/components/body/body.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
