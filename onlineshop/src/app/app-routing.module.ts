af10577e74b37452c5239668f6b75e3fa4447798
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-17 19:16:47 +0300

    Changes for routing

--- onlineshop/src/app/app-routing.module.ts
+++ onlineshop/src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AromaSticks } from '../components/aromasticks/aromasticks.component';
 import { Candles } from '../components/candles/candles.component';
 import { Diffusers } from '../components/diffusers/diffusers.component';
 import { Others } from '../components/others/others.component';
 import { Contacts } from '../components/contacts/contacts.component';
 import { AboutUs } from '../components/aboutus/aboutus.component';
+import { HomeComponent } from '../components/home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'aromasticks', component: AromaSticks },
   { path: 'candles', component: Candles },
   { path: 'diffusers', component: Diffusers },
   { path: 'others', component: Others },
   { path: 'contacts', component: Contacts },
   { path: 'aboutus', component: AboutUs },
-  { path: '*', component: AppComponent }
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
