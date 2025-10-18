b3d8bf62d198a57d8510941622e3bd8360b71005
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-18 10:30:59 +0300

    Working routing

--- onlineshop/src/app/app.routes.ts
+++ onlineshop/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { AromaSticks } from '../components/aromasticks/aromasticks.component';
+import { Candles } from '../components/candles/candles.component';
+import { Diffusers } from '../components/diffusers/diffusers.component';
+import { Others } from '../components/others/others.component';
+import { Contacts } from '../components/contacts/contacts.component';
+import { AboutUs } from '../components/aboutus/aboutus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'aromasticks', component: AromaSticks },
+  { path: 'candles', component: Candles },
+  { path: 'diffusers', component: Diffusers },
+  { path: 'others', component: Others },
+  { path: 'contacts', component: Contacts },
+  { path: 'aboutus', component: AboutUs },
+  { path: '**', component: HomeComponent }
+  
+];
