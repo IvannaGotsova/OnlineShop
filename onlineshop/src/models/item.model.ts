05871d6a1aaf1af68c65960e46124edbb930724a
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-19 10:18:49 +0300

    Added interface

---
+++ onlineshop/src/models/item.model.ts
@@ -0,0 +1,14 @@
+export interface Item {
+  id: number;
+  name: string;
+  product: string;
+  type: string;
+  ingredients: string;
+  fragrance: string;
+  description: string;
+  quantity: number;
+  volume: number;
+  price: number;
+  time: number;
+  photo: string;
+}
