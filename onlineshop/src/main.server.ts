722c2ca6c15e79fa6f6b48358f858728c33855eb
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-19 10:17:10 +0300

    Set provide router settings

--- onlineshop/src/main.server.ts
+++ onlineshop/src/main.server.ts
@@ -1,8 +1,7 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { config as serverConfig } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(AppComponent, config, context);
-
-export default bootstrap;
+export default function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, serverConfig, context);
+}
--- onlineshop/src/main.ts
+++ onlineshop/src/main.ts
@@ -2,8 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    provideRouter(routes)
+  ]
 })
-  .catch(err => console.error(err));
+.catch(err => console.error(err));
