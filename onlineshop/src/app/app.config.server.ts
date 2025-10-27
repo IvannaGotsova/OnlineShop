df96b35ea7a7d3446cab42e66d6168e5a9656b42
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-27 17:34:08 +0200

    Removing server problem

--- onlineshop/src/app/app.config.server.ts
+++ onlineshop/src/app/app.config.server.ts
@@ -1,7 +1,12 @@
 import { provideRouter } from '@angular/router';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const config = {
-  providers: [provideRouter(routes), provideServerRendering()],
+  providers: [
+    provideRouter(routes),
+    provideServerRendering(),
+    provideClientHydration()
+  ],
 };
--- onlineshop/src/main.server.ts
+++ onlineshop/src/main.server.ts
@@ -1,10 +1,17 @@
 import {
+  ApplicationConfig,
   bootstrapApplication,
   BootstrapContext,
 } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config as serverConfig } from './app/app.config.server';
+import { provideClientHydration } from '@angular/platform-browser';
 
+export const config: ApplicationConfig = {
+  providers: [
+    provideClientHydration()
+  ]
+};
 export default function bootstrap(context: BootstrapContext) {
   return bootstrapApplication(AppComponent, serverConfig, context);
 }
